923faa3df3b2e08bf67cc83b1af828dab0869935
mainaGioia <alessandra.manna89@gmail.com>
2017-10-04 15:06:58 +0200

    App.xaml.cs updated

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,27 +1,39 @@
-﻿using XamarinNavy.Views;
+﻿using XamarinNavy.Services.Navigation;
+using XamarinNavy.Views;
+
 
 using Xamarin.Forms;
 
 namespace XamarinNavy
 {
-    public partial class App : Application
-    {
-        public static bool UseMockDataStore = true;
-        public static string BackendUrl = "https://localhost:5000";
+	public partial class App : Application
+	{
+		public static bool UseMockDataStore = true;
+		public static string BackendUrl = "https://localhost:5000";
 
-        public App()
-        {
-            InitializeComponent();
+		public App()
+		{
+			InitializeComponent();
 
-            if (UseMockDataStore)
-                DependencyService.Register<MockDataStore>();
-            else
-                DependencyService.Register<CloudDataStore>();
+			if (UseMockDataStore)
+				DependencyService.Register<MockDataStore>();
+			else
+				DependencyService.Register<CloudDataStore>();
 
-            if (Device.RuntimePlatform == Device.iOS)
-                MainPage = new MainPage();
-            else
-                MainPage = new NavigationPage(new MainPage());
-        }
-    }
+			MainPage = InitNavigation();
+
+		}
+
+
+		Page InitNavigation()
+		{
+			var navigationService = new NavigationService();
+			//return navigationService.InitializeAsync(new LoginPage());
+			return navigationService.InitializeDrawerAsync(new BaseDrawerPage(), new BaseLoginPage());
+			//return navigationService.InitializeTabsAsync(new List<CustomMenuItem> {
+			//    new CustomMenuItem { TargetType = typeof(AboutPage), Title = "About", Icon = "tab_about.png" },
+			//    new CustomMenuItem { TargetType = typeof(ItemsPage), Title = "Browse", Icon = "tab_feed.png" },
+			//});
+		}
+	}
 }
